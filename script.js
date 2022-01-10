90335641321b74d8bebcb529d4a9a134d23cc311
Sedron2 <91390795+Sedron2@users.noreply.github.com>
2022-01-10 00:10:18 -0500

    Automatizando insertacion de cells

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-cells = document.getElementsByTagName("span")
+const div = document.querySelector(".grid")
+div.innerHTML = "<span>Cell</span>\n".repeat(100)
+
+let cells = document.getElementsByTagName("span")
 console.log(cells.length)
